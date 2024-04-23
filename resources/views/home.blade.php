74981c2d45f342677931bac1265849fc43a90b20
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-04-24 00:03:22 +0600

    "Add Bootstrap and login route for Movie Listing App"
    This commit adds Bootstrap as a dependency and includes its CSS in the home page. It also adds a new route for displaying the login form. Note that the corresponding Login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Listing App</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Optional Bootstrap theme -->
+    <!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap-theme.min.css" rel="stylesheet"> -->
 </head>
 
 <body>
-    <div>
-        <h1>Welcome to Movie Listing App</h1>
-        <ul>
-            <li><a href="{{ route('register') }}">Register</a></li>
-            <li><a href="{{ route('login') }}">Login</a></li>
-            <li><a href="{{ route('movies.index') }}">View All Movies</a></li>
-            <li><a href="{{ route('user.favorites') }}">View Favorites</a></li>
+    <div class="container">
+        <h1 class="mt-5">Welcome to Movie Listing App</h1>
+        <ul class="list-group mt-4">
+            <li class="list-group-item"><a href="{{ route('register') }}" class="btn btn-primary">Register</a></li>
+            <li class="list-group-item"><a href="{{ route('login') }}" class="btn btn-success">Login</a></li>
+            <li class="list-group-item"><a href="{{ route('movies.index') }}" class="btn btn-info">View All Movies</a></li>
+            <li class="list-group-item"><a href="{{ route('user.favorites') }}" class="btn btn-warning">View Favorites</a></li>
         </ul>
     </div>
+
+    <!-- Bootstrap JavaScript and dependencies (optional) -->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script> -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
