57e516b25ed5062e5bf324a1848586350a43d475
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-04-24 00:28:42 +0600

    Add favorites method to UserController and User model
    The UserController now includes a favorites method that checks if a user is authenticated and retrieves their favorites from the User model. The User model has a new favorites method that defines a many

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function favorites()
+    {
+        if (auth()->check()) {
+            
+            $user = auth()->user();
+            $favorites = $user->favorites; 
+
+            return view('favorites.index', compact('favorites'));
+        } else {
+            return redirect()->route('login')->with('error', 'You need to login to view your favorites.');
+        }
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'cast',
+        'category',
+        'release_date',
+        'budget',
+    ];
 }
